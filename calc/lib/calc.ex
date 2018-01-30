23af68c5ec8f09735793afd9ab0116ab76bd6979
Cheng Zeng <zeng.chen@husky.neu.edu>
2018-01-30 15:14:55 -0500

    update

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -12,7 +12,36 @@ defmodule Calc do
       :world
 
   """
-  def hello do
-    :world
+
+  def main() do
+    IO.gets("Enter input: ")
+    |> String.split(" ")
+    |> eval([])
+    main()
   end
+
+  def eval(input, processing ) do
+    cond do
+      Enum.empty?(input) ->
+        cond do
+          length(processing) == 1->
+            hd(processing)
+            |> IO.puts()
+          true ->
+            [a,o,b|t] = processing
+            cond do
+              is_number(a) ->
+                cond do
+                  o == "+" ->
+                    eval(input, [a+b] ++ t)
+                  o == "-" ->
+                    eval(input, [a-b] ++ t)
+                end
+              true ->
+                IO.puts("Not implemented")
+            end
+        end
+    end
+  end
+
 end
--- calc/test/calc_test.exs
+++ calc/test/calc_test.exs
@@ -3,6 +3,9 @@ defmodule CalcTest do
   doctest Calc
 
   test "greets the world" do
-    assert Calc.hello() == :world
+    # assert Calc.main("2 + 3") == 5
+    # assert Calc.main("5 * 1") == 5
+    # assert Calc.main("20 / 4") == 5
+    # assert Calc.main("24 / 6 + (5 - 4)") == 5
   end
 end
